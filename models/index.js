79d4c9f18d4165ca96a51af1c8f25847a20fcdc6
Vaibhav <vaibhavt147@gmail.com>
2023-07-04 23:20:22 +0530

    credentials.json added inside config and will later be used to store all the credentials

--- models/index.js
+++ models/index.js
@@ -2,27 +2,12 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
+const config = require("../config/credentials.json");
 
 // TODO : Extract this config from config file. use process.env.NODE_ENV to get the env and using the creds accordingly. For now the config is hardcoded
-const config = {
-  url: "localhost:5432",
-  dialect: "postgres",
-  username: "user1",
-  password: "user1",
-};
 
 // handle if url doen't exist in config
-let sequelize;
-if (config.url) {
-  sequelize = new Sequelize(config.url, config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+const sequelize = new Sequelize(config.development.postgres.options);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
