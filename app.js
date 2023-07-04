51339ca3cee847d95edcf9d63ae1a686481bdbe8
Vaibhav <vaibhavt147@gmail.com>
2023-07-04 22:58:21 +0530

    Minor changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const routes = require("./src/routes");
+const routes = require("./routes");
 const app = express();
 const port = process.env.PORT || 8080;
 
